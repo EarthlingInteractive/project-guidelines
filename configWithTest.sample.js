470d5066670439f88f3a0229c9278c098df59bb6
Jared Chapiewsky <chapiewsky@earthlinginteractive.com>
2017-11-01 12:50:01 -0500

    Some very minor tweaks and cleanup to make the guidelines EI specific

--- configWithTest.sample.js
+++ configWithTest.sample.js
@@ -1,4 +1,4 @@
-const joi = require('joi')
+const joi = require('joi');
 
 const envVarsSchema = joi.object({  
   NODE_ENV: joi.string()
@@ -16,11 +16,11 @@ const envVarsSchema = joi.object({
     .falsy('false')
     .default(true)
 }).unknown()
-  .required()
+  .required();
 
-const { error, value: envVars } = joi.validate(process.env, envVarsSchema)  
+const { error, value: envVars } = joi.validate(process.env, envVarsSchema);
 if (error) {  
-  throw new Error(`Config validation error: ${error.message}`)
+  throw new Error(`Config validation error: ${error.message}`);
 }
 
 const config = {  
@@ -35,6 +35,6 @@ const config = {
     port: envVars.PORT
   }
   // ...
-}
+};
 
 module.exports = config;
